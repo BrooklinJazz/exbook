320e2b2c023437800f687859480cb1b145a3f353
Ryan Winchester <ryan@sevenshores.co>
2022-09-14 12:21:23 -0300

    move requirements to attribute

--- lib/mix/tasks/notebooks.ex
+++ lib/mix/tasks/notebooks.ex
@@ -1,15 +1,14 @@
 defmodule Mix.Tasks.Notebooks do
   use Mix.Task
 
+  @requirements ["app.start"]
+
   def run(_args) do
     opts =
       case File.read(".exbooks.exs") do
         {:ok, file} -> :erlang.binary_to_term(file)
         _ -> [path: "./notebooks", ignore: []]
       end
-
-    Mix.Task.run("app.start")
-
 
     app = Mix.Project.config()[:app]
     ExBook.app_to_exbook(app, opts)
